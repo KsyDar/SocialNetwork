41e5b9864f7b6e2f0d969bef1062cea3a4e89307
KsyDar <ksydar2@gmail.com>
2022-09-04 02:24:07 +0300

    add ToDoList and ToDoLists state

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,27 +1,99 @@
 <template>
   <div class="to-do-list-wrapper">
-      <h3 class="to-do-list__title">Список дел</h3>
-      <ul class="to-do-list">
-        <li class="to-do-list__item" v-for="toDo of toDos" :key="toDo.toDoId">
-          <p class="to-do-list__item__title">
-            {{ toDo.title }}
-          </p>
-          <div class="to-do-list__item__buttons">
-            <button>Изменить</button>
-            <button>Удалить</button>
-          </div>
-        </li>
-      </ul>
-      <button>Добавить</button>
-    </div>
+    <h3 class="to-do-list__title">Список дел</h3>
+    <ol class="to-do-list">
+      <li class="to-do-list__item" v-for="toDo of toDos" :key="toDo.toDoId">
+        <label class="to-do-list__item__title" :class="{ 'is-done': toDo.isDone }">
+          <input type="checkbox" v-model="toDo.isDone">
+          {{ toDo.title }}
+        </label>
+        <div class="to-do-list__item__buttons">
+          <button class="to-do-list__button to-do-list__button--change">
+            Изменить
+          </button>
+          <button class="to-do-list__button to-do-list__button--delete">
+            Удалить
+          </button>
+        </div>
+      </li>
+    </ol>
+    <button class="to-do-list__button to-do-list__button--add">Добавить</button>
+  </div>
 </template>
 
 <script setup>
- const props = defineProps({
-    toDos: Array,
- })
+import { ref } from "vue";
+
+const props = defineProps({
+  toDos: Array,
+});
 </script>
 
 <style>
+.to-do-list-wrapper {
+  margin-top: 4rem;
+  border: solid 3px #8864c6;
+  border-radius: 10px;
+}
 
+.to-do-list__title {
+  margin: 1rem;
+  font-size: 2rem;
+  font-weight: 600;
+  color: #6e4fa3;
+  text-align: center;
+}
+
+.to-do-list {
+  padding: 0;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+}
+
+.to-do-list__item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: solid 2px #aa94cf4f;
+  margin-bottom: 1rem;
+}
+
+.is-done {
+  text-decoration: line-through;
+}
+
+.to-do-list__button {
+  padding: 3px 7px;
+  border-radius: 7px;
+  font-weight: 600;
+}
+
+.to-do-list__button:hover {
+  cursor: pointer;
+  background: #00000059;
+}
+
+.to-do-list__button--change {
+  margin-right: 2rem;
+  border: solid 2px #8864c6;
+  background: #927dd2;
+  color: #fff;
+}
+
+.to-do-list__button--change:hover {
+  background: #60458f;
+}
+
+.to-do-list__button--add {
+  margin: 2rem auto;
+  display: block;
+  margin: 2rem auto;
+  display: block;
+  border: solid 2px #8864c6;
+  color: #8864c6;
+}
+
+.to-do-list__button--add:hover {
+  background: #dac4eb;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="main-content">
-    <h1>{{ user.name }}</h1>
-    <ToDoList :toDos="toDos" />
+    <div class="home-wrapper">
+      <div class="home__profile">
+        <h1 class="profile__title">{{ user.name }}</h1>
+        <img class="profile__avatar" :src="user.avatar" />
+        <p class="profile__information">Возраст: {{ user.age }}</p>
+        <p class="profile__information">О себе: {{ user.description }}</p>
+        <p class="profile__information">Друзья ({{ friendList.length }})</p>
+        <!-- <router-link >Друзья ({{user.friendList.length}})</router-link> -->
+        <button class="profile__button">Редактировать</button>
+      </div>
+      <ToDoList :toDos="toDos" />
+    </div>
   </div>
 </template>
 
@@ -9,7 +19,7 @@
 import { onBeforeMount, ref } from "vue";
 import { useUserStore } from "../../platform/store/users/users";
 import { useToDoListsStore } from "../../platform/store/toDoLists/toDoLists";
-import ToDoList from "../components/toDoList.vue";
+import ToDoList from "./../components/ToDoList.vue";
 
 const props = defineProps({
   id: String,
@@ -19,13 +29,29 @@ const userStore = useUserStore();
 const toDoListStore = useToDoListsStore();
 
 const user = ref({});
+const friendList = ref([]);
 const toDos = ref([]);
 
 onBeforeMount(async () => {
   toDos.value = await toDoListStore.getToDoList(props.id);
   user.value = userStore.currentUser;
+  friendList.value = userStore.currentUser.friendList;
 });
 </script>
 
 <style>
+.home-wrapper {
+  padding: 2rem 1rem;
+}
+
+.profile__title {
+  margin-top: 0;
+  font-size: 2.5rem;
+}
+
+.profile__avatar {
+  width: 20rem;
+  border-radius: 50%;
+  border: solid 2px #6629cd;
+}
 </style>
